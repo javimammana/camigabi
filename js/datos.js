85b9bc99899db69c9348612faa24e21f87d5159d
javimammana <javiermammana.dev@gmail.com>
2025-07-07 01:11:42 -0300

    correccion mapa

--- js/datos.js
+++ js/datos.js
@@ -2,6 +2,6 @@ const fecha = new Date("07/13/2025 13:30");
 const cumple = new Date("07/13/2025");
 const horaEvento = "13:30Hs";
 const direccionLink = "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d163.43916626087736!2d-64.29040400067616!3d-31.122375221634154!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1ses!2sar!4v1751856047173!5m2!1ses!2sar";
-const coordenadas = "Mendoza 376, Villa Sol, Salsipuedes, Córdoba X5113";
+const coordenadas = "-31.122410, -64.290415";
 const formAsistencia = "https://wa.me/5493522408735";
 const formCanciones = "https://forms.gle/D9trxxRy8nJUSyTZ8";
